97b95a1c3589456c1e3409b1ae3ee4c0ee1c10ff
Rungsikorn Rungsikavanich <rungsikorn@icloud.com>
2018-11-15 16:23:57 +0700

    update todo example

---
+++ todos-react-hooks/precache-manifest.dc72dd2bdc670009fd52e1cc50cb0380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "fe25db4a5d565dad3c4e",
+    "url": "./static/js/main.fe25db4a.chunk.js"
+  },
+  {
+    "revision": "60566f14d5ae9571109e",
+    "url": "./static/js/1.60566f14.chunk.js"
+  },
+  {
+    "revision": "fe25db4a5d565dad3c4e",
+    "url": "./static/css/main.92048d74.chunk.css"
+  },
+  {
+    "revision": "60566f14d5ae9571109e",
+    "url": "./static/css/1.54bdfa7d.chunk.css"
+  },
+  {
+    "revision": "7f7df70fe20c7eec1c5c81180bd55624",
+    "url": "./index.html"
+  }
+];
--- todos-react-hooks/service-worker.js
+++ todos-react-hooks/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.380e8cd72905576403ec332aac7bdd62.js"
+  "./precache-manifest.dc72dd2bdc670009fd52e1cc50cb0380.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
